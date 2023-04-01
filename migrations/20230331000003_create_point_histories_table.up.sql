d93b68ec5868e87d85131d9738cc86bc5dd3e3f0
Mac <rocked.778@gmail.com>
2023-04-02 00:01:16 +0900

    fix

--- migrations/20230331000003_create_point_histories_table.up.sql
+++ migrations/20230331000003_create_point_histories_table.up.sql
@@ -2,8 +2,8 @@ CREATE TABLE point_histories (
     id INT IDENTITY(1,1) PRIMARY KEY,
     user_id INT NOT NULL,
     point INT NOT NULL,
-    action VARCHAR(255) NOT NULL,
-    details VARCHAR(255) NOT NULL,
+    action NVARCHAR(255) NOT NULL,
+    details NVARCHAR(255) NOT NULL,
     created_at DATETIME2 NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
